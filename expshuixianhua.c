d81efd73c43df891fc91c53e70af72144c264997
PeterWilliams1999 <48818305+PeterWilliams1999@users.noreply.github.com>
2019-03-22 13:16:33 +0800

    Add files via upload

---
+++ expshuixianhua.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main() {
+	for(int i=100,m,n,k;i<1000;i++){
+		k = i % 10;
+		n = (i - k) / 10 % 10;
+		m = (i - 10 * n - k) / 100;
+		if (m * m * m + n * n * n + k * k * k == i) {
+			printf("%d", i);
+		}
+		
+	}
+	getchar();
+	getchar();
+	return 0;
+
+}
