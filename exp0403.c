5be4242e227cb8f3b1d0cb87c0d9cbb333ddcbbd
PeterWilliams1999 <48818305+PeterWilliams1999@users.noreply.github.com>
2019-03-22 13:24:32 +0800

    Update exp0403.c

--- exp0403.c
+++ exp0403.c
@@ -1,11 +1,11 @@
 //exp0403 Written By Zhuzhenwu 071840214
 #include <stdio.h>
-int main(){
-    double x,y;
-    scanf_s("%lf%lf",&x,&y);
-    if(x>0&&y>0) printf('1');
-    if(x<0&&y>0) printf('2');
-    if(x<0&&y<0) printf('3');
-    if(x>0&&y<0) printf('4');
-    if(x==0||y==0) printf('5');
+int main() {
+	double x, y;
+	scanf_s("%lf%lf", &x, &y);
+	if (x > 0 && y > 0) printf("1");
+	if (x < 0 && y>0) printf("2");
+	if (x < 0 && y < 0) printf("3");
+	if (x > 0 && y < 0) printf("4");
+	if (x == 0 || y == 0) printf("5");
 }
