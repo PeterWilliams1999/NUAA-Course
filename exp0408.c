1330d8370041588c86152f5c7ce56b6b3cb20912
PeterWilliams1999 <48818305+PeterWilliams1999@users.noreply.github.com>
2019-03-22 14:09:17 +0800

    Update exp0408.c

--- exp0408.c
+++ exp0408.c
@@ -2,10 +2,10 @@
 #include<stdio.h>
 int main(){
     int n;
-    double sum=0,outputsum;
+    double sum=1,outputsum;
     scanf_s("%d",&n);
     for(int i=1;i<=n;i++){
-    sum+=((2*i)*(2*i))/((2*i-1)/(2i*+1));
+    sum*=((2*i)*(2*i))/((2*i-1)/(2i*+1));
     }
     outputsum=2*sum;
     printf("%lf",outputsum);
