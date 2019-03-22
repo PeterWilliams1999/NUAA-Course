41e6ecfbfe9f9c6a05abfaa95c379e8ac9577ab4
PeterWilliams1999 <48818305+PeterWilliams1999@users.noreply.github.com>
2019-03-22 14:44:35 +0800

    Update exp0410.c

--- exp0410.c
+++ exp0410.c
@@ -1,28 +1,34 @@
 //exp0410 Written By Zhuzhenwu 071840214
 #include <stdio.h>
 
-int Fib1(var){
-if(var==0) return 0;
-if(var==1) return 1;
-if(var>=2){
-    return(Fib1(var-2)+Fib2(var-1));
-}
-}
-
-int Fib2(var){
-if(var==1) return 1;
-if(var==2) return 1;
-if(var>=3) {
-    return (Fib2(var-2)+Fib(var-1));
-}
+double Fib1(double var) {
+	if (var == 0) return 1.0;
+	if (var == 1) return 1.0;
+	if (var >= 2) {
+		double s1 = Fib1(var - 2);
+		double s2 = Fib1(var - 1);
+		double output = s1 + s2;
+		return output;
+	}
 }
 
-int main(){
-    double i,k,sum=0;
-    for(int num=1;num<=20;num++){
-        i=Fib1(num);
-        k=Fib2(num);
-        sum+=i/j;
-    }
-printf("%lf",sum);
+double Fib2(double var) {
+	if (var == 1) return 1.0;
+	if (var == 2) return 1.0;
+	if (var >= 3) {
+		double s1 = Fib2(var - 2);
+		double s2 = Fib2(var - 1);
+		double output = s1 + s2;
+		return output;
+	}
+}
+
+int main() {
+	double i, k, sum = 0;
+	for (double num = 1; num <= 20; num++) {
+		i = Fib1(num);
+		k = Fib2(num);
+		sum += (i / k);
+	}
+	printf("%lf", sum);
 }
